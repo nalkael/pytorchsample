78a078f7a65df529d6a18f20e33839e5c6705bda
nalkae <nalkae2003@gmail.com>
2024-04-17 17:11:36 +0200

    modify image classification function

--- imageclassifier.py
+++ imageclassifier.py
@@ -1,7 +1,67 @@
 import torch
+import torch.utils
+import torch.utils.data
 import torchvision
 import torchvision.transforms as tf
 
 # the output of torchvision datasets are PILImage images of range [0,1]
 from PIL import Image
 
+transform = tf.Compose([
+    tf.Resize((128, 128)),
+    tf.ToTensor(), # Convert the image to a PyTorch Tensor
+    tf.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+])
+
+batch_size = 6
+trainset = torchvision.datasets.CIFAR10(root="./data", train=True, 
+                                          download=True,transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, 
+                                          shuffle=True, num_workers=2)
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# functions to show an image
+
+
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+# get some random training images
+dataiter = iter(trainloader)
+images, labels = next(dataiter)
+
+# show images
+imshow(torchvision.utils.make_grid(images))
+# print labels
+print(' '.join(f'{classes[labels[j]]:5s}' for j in range(batch_size)))
+
+# define a convolutional neural network
+import torch.nn as nn
+import torch.nn.functional as func
+
+class ConvNet(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(func.relu(self.conv1(x)))
+        x = self.pool(func.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = func.relu(self.fc1(x))
+        x = func.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
