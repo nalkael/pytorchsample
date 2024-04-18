5b1dd42296d173ac7919594de502effe4b3047e4
nalkae <nalkae2003@gmail.com>
2024-04-18 16:07:31 +0200

    add simple test of embedding

--- embedding_test.py
+++ embedding_test.py
@@ -8,3 +8,15 @@ ft = fasttext.load_model('cc.en.300.bin')
 model_load_end = time.time()
 print('model loaded... in %.2f second' % (model_load_end - model_load_start))
 
+# embed the text into vectors
+train_file_path = './movies-funny.train.tsv'
+test_file_path = './movies-funny.test.tsv'
+
+train_dict_list = []
+
+with open(train_file_path, 'r') as file:
+    for line_num, line in enumerate(file, start=1):
+        #if line_num <= 20: 
+        #    print(f"Line {line_num}: {line.strip()}")
+        # TODO
+        pass
