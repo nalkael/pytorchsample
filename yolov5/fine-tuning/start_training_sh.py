faff3a6e72209c919fa53f5ef6295a3ef3a52404
nalkae <nalkae2003@gmail.com>
2024-06-03 17:04:35 +0200

    add yolov5 model and some test

---
+++ yolov5/fine-tuning/start_training_sh.py
@@ -0,0 +1,13 @@
+import os
+# install YOLOv5 dependencies
+
+# Download pre-trained YOLOv5 model weights
+
+# Path to the datasets YAML configuration
+custom_dataset_yaml_path = '/home/rdluhu/Dokumente/yolov5/data/custom_dataset.yaml'
+
+
+'''
+python train.py --img 640 --batch 16 --epochs 100 --data data/custom_dataset.yaml --weights yolov5x.pt --cfg models/yolov5x.yaml --name custom_yolov5x
+'''
+os.system(f'python train.py --img 640 --batch 16 --epochs 100 --data {custom_dataset_yaml_path} --weights yolov5x6.pt --cfg models/yolov5x6.yaml --name custom_yolov5x6')
